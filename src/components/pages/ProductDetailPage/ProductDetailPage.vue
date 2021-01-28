baf247f05c189b340fb056372f381a786317cb1e
Benson Wu <rhualikescode@gmail.com>
2021-01-28 14:08:42 +0800

    To build recommand row

--- src/components/pages/ProductDetailPage/ProductDetailPage.vue
+++ src/components/pages/ProductDetailPage/ProductDetailPage.vue
@@ -2,10 +2,19 @@
     <div class="product-detail-page">
         <div class="product-attribute default-shadow">
             <div class="img-block">
-                <img src="/images/goku2.jpg" alt="">
+                <img :src="productImgs[currentShowsImgIndex]" alt="product image">
+            </div>
+            <div class='img-dots-block'>
+                <div 
+                    v-for="(item, index) in productImgs" 
+                    :key="index" 
+                    class='dots'
+                    :class="[currentShowsImgIndex === index ? 'default-deep-green-bgc' : 'origin-dot-color']"
+                    @click="toggleCurrentShowsImg(index)"
+                ></div>
             </div>
             <div class="spec-block">
-                <product-spec :product_data='quickAddProductData'/>
+                <product-spec :product_data='productData'/>
             </div>
         </div>
         <detail-nav-row @toggle-product-info='toggleProductInfo'/>
@@ -39,7 +48,9 @@ export default {
                 id: '',
                 type: ''
             },
-            quickAddProductData: {
+            productImgs: ['/images/goku2.jpg', '/images/goku4.jpeg', '/images/goku3.jpeg'],
+            currentShowsImgIndex: 0,
+            productData: {
                 name: 'Qucik Product Name',
                 spec: {
                     spec_name: 'Color',
@@ -83,12 +94,15 @@ export default {
     mounted(){
         this.productAttr.id = this.$route.params.id;
         this.productAttr.type = this.$route.params.type;
-        this.quickAddProductData.name = this.productAttr.type + ':' + this.productAttr.id;
+        this.productData.name = this.productAttr.type + ':' + this.productAttr.id;
         this.showProductInfo = 'description'
     },
     methods: {
         toggleProductInfo(value){
             this.showProductInfo = value
+        },
+        toggleCurrentShowsImg(index){
+            this.currentShowsImgIndex = index
         }
     }
 }
@@ -102,7 +116,9 @@ export default {
         padding: 1.5rem
         border-radius: 5px
     .img-block
-        // width: 75vw
+        width: 95%
+        height: 20rem
+        overflow: hidden
         margin: 1rem auto 0 auto 
         img
             width: 100%
@@ -113,4 +129,17 @@ export default {
         background-color: #fff
         padding: 2rem 1rem 10rem 1rem
         border-radius: 0 0 5px 5px
+    .img-dots-block
+        display: flex
+        align-items: center
+        justify-content: center
+        margin-top: 1rem
+    .dots
+        width: .7rem
+        height: .7rem
+        // background-color: rgba(0,0,0, .1)
+        border-radius: 50%
+        margin: 0 .5rem
+    .origin-dot-color
+        background-color: rgba(0,0,0,.1)   
 </style>
